05fe49d1afa45ddfcd20d9945612d603b317a245
ETI <ETI@PO-21499>
2022-09-07 18:40:49 -0400

    affichage debut combat

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -22,10 +22,11 @@ public class JeuGladiateurs {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
-    // TODO : Afficher les infos de chaque personnage
+    
     bob.afficherInfosPersonnage();
     igor.afficherInfosPersonnage();
-    // TODO : Afficher le message du début du combat
+    
+    affichage.afficherDebutCombat();
     // </editor-fold>
 
     // **************************************************************************
