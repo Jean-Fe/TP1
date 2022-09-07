6b9aa890b89764ecaa1abf90c1f2c0f11b5dd2f0
ETI <ETI@PO-21499>
2022-09-07 19:20:20 -0400

    set random initiative

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -134,7 +134,12 @@ public class Personnage {
     }
 
     public void setNewInitiativeRandom() {
-        // TODO : Modifier de façon aléatoire la valeur INI du personnage.
+        
+        Random rand = new Random();
+
+    int minValue = 0;
+    int maxValue = 100;
+    this.setInitiative(rand.nextInt(maxValue - minValue) + minValue);
     }
     // </editor-fold>
 }
