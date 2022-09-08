5f44a88bd674fccbc4f59a347f7d0e6bf2c9b725
ETI <ETI@PO-21499>
2022-09-08 15:23:05 -0400

    boucle combat

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -28,6 +28,7 @@ public class JeuGladiateurs {
     
     affichage.afficherDebutCombat();
     
+    
     // </editor-fold>
 
         
@@ -36,6 +37,25 @@ public class JeuGladiateurs {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
     // TODO : La boucle contenant les étapes du combat
+        do {
+            
+    tour.afficheTour();
+    
+        for (int i = 0; i < 100; i++) {
+            if (personnage1.getInitiative()==i) personnage1.frapperPersonnage(personnage2);
+            if (personnage2.getInitiative()==i) personnage2.frapperPersonnage(personnage1);
+        }
+        affichage.afficherSeparateurInfosPerso();
+        
+        personnage1.afficherInfosPersonnage();
+        personnage2.afficherInfosPersonnage();
+        personnage1.setNewInitiativeRandom();
+        personnage2.setNewInitiativeRandom();
+        
+        tour.augmenteTour();
+        
+        affichage.afficherSeparateurDeTour();
+        } while (personnage1.getPointsDeVie()>0 && personnage2.getPointsDeVie()>0);
     // TODO : Après la boucle, afficher le résultat du combat
     // </editor-fold>
     }
